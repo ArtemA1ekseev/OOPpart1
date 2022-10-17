a1e67163fdf3630a88447db5afc1c1b057ac3f52
ArtemA1ekseev <a1ekseevartem.ru@gmail.com>
2022-10-17 23:19:03 +0400

    Домашнее задание 2. Задание 1 - 2.

--- src/homework1/Car.java
+++ src/homework1/Car.java
@@ -9,36 +9,12 @@ public class Car {
     public String productionCountry;
     // Задание 2.
     public Car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry) {
-        if (brand == null) {
-            this.brand = "default";
-        } else {
-            this.brand = brand;
-        }
-        if (model == null) {
-            this.model = "default";
-        } else {
-            this.model = model;
-        }
-        if (productionCountry == null) {
-            this.productionCountry = "default";
-        } else {
-            this.productionCountry = productionCountry;
-        }
-        if (Double.compare(engineVolume, 0) == 0) {
-            this.engineVolume = 1.5;
-        } else {
-            this.engineVolume = engineVolume;
-        }
-        if (color == color) {
-            this.color = "белый";
-        } else {
-            this.color = color;
-        }
-        if (productionYear == 0) {
-            this.productionYear = 2000;
-        } else {
-            this.productionYear = productionYear;
-        }
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.productionYear = productionYear;
+        this.productionCountry = productionCountry;
     }
 }
 
