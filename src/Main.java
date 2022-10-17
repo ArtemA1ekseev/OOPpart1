06ee22c2685db6dc6c46c9f77950eba155f29ae4
ArtemA1ekseev <a1ekseevartem.ru@gmail.com>
2022-10-17 23:14:02 +0400

    Домашнее задание 3. Задание 1 - 2.

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,10 @@ public class Main {
         Human maksim = new Human(1987, "Максим", "Минск", "бренд-менеджер");
 
         // Задание 1.
-        Human anna = new Human(1993, "Аня", "Москва", "методист образовательных программ");
+        Human anna = new Human(1993, "Аня", null, "методист образовательных программ");
 
         // Задание 1.
-        Human katya = new Human(1994, "Катя", "Калининград", "продакт-менеджер");
+        Human katya = new Human(-1994, "Катя", "Калининград", "продакт-менеджер");
 
         // Задание 1.
         Human artem = new Human(1995, "Артем", "Москва", "директор по развитию бизнеса");
@@ -24,23 +24,23 @@ public class Main {
         System.out.println(" ");
 
         // Задание 2.
-        Car ladaGranta = new Car("Lada", "Granta", 1.7, "желтый", 2015, "России");
+        Car ladaGranta = new Car("Lada", null, 1.7, "желтый", 2015, "России");
         System.out.println("Бренд автомобиля — " + ladaGranta.brand + ", модель — " + ladaGranta.model + ", сборка в " + ladaGranta.productionCountry + ", цвет кузова — " + ladaGranta.color + ", объем двигателя — " + ladaGranta.engineVolume + " л, год выпуска — " + ladaGranta.productionYear + ".");
 
         // Задание 2.
-        Car audiA8 = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020, "Германии");
+        Car audiA8 = new Car("Audi", "A8 50 L TDI quattro", 0, "черный", 2020, "Германии");
         System.out.println("Бренд автомобиля — " + audiA8.brand + ", модель — " + audiA8.model + ", сборка в " + audiA8.productionCountry + ", цвет кузова — " + audiA8.color + ", объем двигателя — " + audiA8.engineVolume + " л, год выпуска — " + audiA8.productionYear + ".");
 
         // Задание 2.
-        Car bmwZ8 = new Car("BMW", "Z8", 3.0, "черный", 2021, "Германии");
+        Car bmwZ8 = new Car("BMW", "Z8", 3.0, null, 2021, "Германии");
         System.out.println("Бренд автомобиля — " + bmwZ8.brand + ", модель — " + bmwZ8.model + ", сборка в " + bmwZ8.productionCountry + ", цвет кузова — " + bmwZ8.color + ", объем двигателя — " + bmwZ8.engineVolume + " л, год выпуска — " + bmwZ8.productionYear + ".");
 
         // Задание 2.
-        Car kiaSportage = new Car("Kia", "Sportage 4-го поколения", 2.4,"красный", 2018,"Южной Корее");
+        Car kiaSportage = new Car("Kia", "Sportage 4-го поколения", 2.4,"красный", 0,null);
         System.out.println("Бренд автомобиля — " + kiaSportage.brand + ", модель — " + kiaSportage.model + ", сборка в " + kiaSportage.productionCountry + ", цвет кузова — " + kiaSportage.color + ", объем двигателя — " + kiaSportage.engineVolume + " л, год выпуска — " + kiaSportage.productionYear + ".");
 
         // Задание 2.
-        Car hyundaiAvante = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южной Корее");
+        Car hyundaiAvante = new Car(null, "Avante", 1.6, "оранжевый", 2016, "Южной Корее");
         System.out.println("Бренд автомобиля — " + hyundaiAvante.brand + ", модель — " + hyundaiAvante.model + ", сборка в " + hyundaiAvante.productionCountry + ", цвет кузова — " + hyundaiAvante.color + ", объем двигателя — " + hyundaiAvante.engineVolume + " л, год выпуска — " + hyundaiAvante.productionYear + ".");
     }
 }
--- src/homework1/Human.java
+++ src/homework1/Human.java
@@ -9,9 +9,25 @@ public class Human {
     // Задание 1.
 
     public Human(int yearOfBirth, String name, String town, String job) {
-        this.yearOfBirth = yearOfBirth;
-        this.name = name;
-        this.town = town;
-        this.job = job;
+        if (yearOfBirth < 0){
+            this.yearOfBirth = 0;
+        } else {
+            this.yearOfBirth = yearOfBirth;
+        }
+        if (name == null) {
+            this.name = "Информация не указана";
+        } else {
+            this.name = name;
+        }
+        if (town == null) {
+            this.town = "Информация не указана";
+        } else {
+            this.town = town;
+        }
+        if (job == null) {
+            this.job = "Информация не указана";
+        } else {
+            this.job = job;
+        }
     }
 }
